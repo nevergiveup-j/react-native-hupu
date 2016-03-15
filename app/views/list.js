945362f2f0d9676fc7e360321571345262fa3f45
nevergiveup-j <nevergivejun@163.com>
2016-03-15 13:31:22 +0800

    refs native tab

---
+++ app/views/list.js
@@ -0,0 +1,87 @@
+'use strict';
+
+import React, {
+  StyleSheet,
+  TabBarIOS,
+  Text,
+  View
+} from 'react-native';
+
+
+var List = React.createClass({
+  statics: {
+    title: '<TabBarIOS>',
+    description: 'Tab-based navigation.',
+  },
+
+  displayName: 'TabBarExample',
+
+  getInitialState: function() {
+    return {
+        selectedTab: 'redTab',
+        notifCount: 0,
+        presses: 0,
+    };
+  },
+
+  _renderContent: function(color: string, pageText: string, num?: number) {
+    return (
+      <View style={[styles.tabContent, {backgroundColor: color}]}>
+        <Text style={styles.tabText}>{pageText}</Text>
+        <Text style={styles.tabText}>{num} re-renders of the {pageText}</Text>
+      </View>
+    );
+  },
+  render: function () {
+    return (
+      <TabBarIOS
+        tintColor="white"
+        barTintColor="darkslateblue">
+        <TabBarIOS.Item
+          title="Blue Tab"
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}>
+          {this._renderContent('#414A8C', 'Blue Tab')}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          systemIcon="history"
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+              notifCount: this.state.notifCount + 1,
+            });
+          }}>
+          {this._renderContent('#783E33', 'Red Tab', this.state.notifCount)}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title="More"
+          onPress={() => {
+            this.setState({
+              selectedTab: 'greenTab',
+              presses: this.state.presses + 1
+            });
+          }}>
+          {this._renderContent('#21551C', 'Green Tab', this.state.presses)}
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  tabContent: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  tabText: {
+    color: 'white',
+    margin: 50,
+  },
+});
+
+
+
+export default List;
--- index.ios.js
+++ index.ios.js
@@ -7,46 +7,30 @@ import React, {
   AppRegistry,
   Component,
   StyleSheet,
+  NavigatorIOS,
   Text,
   View
 } from 'react-native';
 
-class hupu extends Component {
-  render() {
+import NewsListView from './app/views/list';
+
+const hupu = React.createClass({
+  render: function () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        barTintColor = '#c01e2f'
+        titleTextColor = '#fff'
+        initialRoute = {{
+          title: '虎扑',
+          component: NewsListView,
+        }}
+      />
     );
   }
-}
+})
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  
 });
 
 AppRegistry.registerComponent('hupu', () => hupu);
